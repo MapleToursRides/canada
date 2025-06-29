558599c5c5ba70d9a7085a4f2c0aad44421b5fbe
MapleToursRides <mapletoursrides@gmail.com>
2025-06-29 19:31:12 -0400

    Update script.js
    Added Google form doc link

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 // script.js for Maple Tours Rides
 function redirectToForm() {
-  window.open("https://forms.gle/your-google-form-link", "_blank");
+  window.open("https://docs.google.com/forms/d/18hj4sUd-bjiVxLDhJiaa6zBjVVd4olIwltVnbpx8lnM/edit", "_blank");
 }
